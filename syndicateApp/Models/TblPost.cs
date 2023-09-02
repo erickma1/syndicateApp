b15528632815ccebc0c7121c518e1f06240475d6
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-09-02 17:02:31 +0000

    Create TblPost.cs

---
+++ syndicateApp/Models/TblPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace syndicateApp.Models;
+
+public partial class TblPost
+{
+    public int IdNo { get; set; }
+
+    public int PostCategory { get; set; }
+
+    public string? PostText { get; set; }
+
+    public string? PostImage { get; set; }
+
+    public int IsActive { get; set; }
+
+    public string? MetaData { get; set; }
+}
