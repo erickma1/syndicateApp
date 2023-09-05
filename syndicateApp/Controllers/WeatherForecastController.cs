41d55dabcf992ba80016b44a37d1ab5ef6c01fef
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-09-05 22:01:44 +0000

    Update WeatherForecastController.cs

--- syndicateApp/Controllers/WeatherForecastController.cs
+++ syndicateApp/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace syndicateApp.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
